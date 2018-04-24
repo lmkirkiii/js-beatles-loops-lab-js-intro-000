2801f72057d10693cb1f33188e5db268d368aeb0
lmkirkiii@gmail.com Kirk <lmkirkiii@gmail.com>
2018-04-24 02:26:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 function theBeatlesPlay(musicians, instruments){
   var empty = [];
   for(index=0; index < musicians.length; index++){
-    
+    empty.push(musicians[index] + " " + "plays" + " " + instruments[index]);
   }
+  return empty
 }
